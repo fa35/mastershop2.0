5eacd2b9e497fea7e6f49dce074ea84b89fd0fdc
luxfinis <luxfinis@ruggedinbox.com>
2015-03-02 11:55:05 +0100

    fixed bugfix

--- Source/MasterShop20.Website/Global.asax.cs
+++ Source/MasterShop20.Website/Global.asax.cs
@@ -22,6 +22,7 @@ namespace MasterShop20.Website
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+     
         }
     }
 }
