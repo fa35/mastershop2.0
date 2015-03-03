c61c93ec5847192d8cad3f3b33a8deb0742b772a
luxfinis <luxfinis@ruggedinbox.com>
2015-03-03 20:13:24 +0100

    added now needed stuff

--- Source/MasterShop20.Website/App_Start/BundleConfig.cs
+++ Source/MasterShop20.Website/App_Start/BundleConfig.cs
@@ -4,16 +4,15 @@ namespace MasterShop20.Website
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootquery")
+            //    .Include("~/Scripts/jquery/jquery-2.1.3.min.js")
+            //    .Include("~/Scripts/bootstrap/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/bootstrap/style/stuff")
-                .Include("~/Content/bootstrap/css/bootstrap-theme.min.css")
-                .Include("~/Content/bootstrap/css/bootstrap.min.css"));
+            //bundles.Add(new StyleBundle("~/bootstrap/style/stuff")
+            //    .Include("~/Content/bootstrap/css/bootstrap-theme.min.css")
+            //    .Include("~/Content/bootstrap/css/bootstrap.min.css"));
         }
     }
 }
