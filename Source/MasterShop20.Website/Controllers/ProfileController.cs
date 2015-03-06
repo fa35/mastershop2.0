563aace746d8188a979f08a187a4dc4a4d80ca32
luxfinis <luxfinix@ruggedinbox.com>
2015-03-06 18:52:36 +0100

    start to create a simple form

--- Source/MasterShop20.Website/Controllers/ProfileController.cs
+++ Source/MasterShop20.Website/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MasterShop20.Website.Models;
 
 namespace MasterShop20.Website.Controllers
 {
@@ -13,7 +14,7 @@ namespace MasterShop20.Website.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return View(new Registration());
         }
 
     }
